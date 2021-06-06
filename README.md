828f2722c926c75a4530215883c33f35de1165c5
Dan Manners <daniel.a.manners@gmail.com>
2021-06-05 23:39:23 -0400

    Adding additional comments.

--- README.md
+++ README.md
@@ -9,10 +9,26 @@ I guess I'll find out soon! 🙂
 ## How to run everything
 
 1. Replace your variables in `environment/cloud.tfvars` with the relevant and appropriate values for your environment.
-2. Run `terraform plan -var-file="environment/cloud.tfvars"` to check the plan.
-3. Run `terraform apply -var-file="environment/cloud.tfvars"` to create the resources.
+2. Run `terraform init`
+3. Run `terraform plan -var-file="environment/cloud.tfvars"` to check the plan.
+4. Run `terraform apply -var-file="environment/cloud.tfvars"` to create the resources.
+
+## How to tear it all down
+
+1. Run `terraform destroy -var-file="environment/cloud.tfvars"`
+
+If it fails the first time, run it a second (or third) time.
+
+## Random things that were a bit challenging
+
+* I'm definitely not a huge fan of how Google Cloud handles Ingress/Egress firewalls vs Security Groups, but perhaps I'll come to love it.
+
+## To-Do
+
+* Add a Load Balancer [https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_forwarding_rule]
 
 ## References
 
-- [Adding SSH Keys in VM Metadata](https://github.com/hashicorp/terraform/issues/6678)
-- [Terraform Compute Reference](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance)
+* [Adding SSH Keys in VM Metadata](https://github.com/hashicorp/terraform/issues/6678)
+* [Terraform Compute Reference](https://registry.terraform.io/providers/hashicorp/google/latest/docs/)
+* [Terraform Dynamics](https://www.terraform.io/docs/language/expressions/dynamic-blocks.html); used for the NAT Router section
--- us-east4.tf
+++ us-east4.tf
@@ -1,10 +1,12 @@
-###############################################################################
+#########################################################################
 ### NETWORKING
+// Create the VPC
 resource "google_compute_network" "vpc" {
   name                    = "vpc-network"
   auto_create_subnetworks = false
 }
 
+// Create the Public Subnets
 resource "google_compute_subnetwork" "public_subnet" {
   for_each = { for k,v in var.networking["vpc_public_subnets"] : k => v }
   name     = each.key
@@ -13,6 +15,7 @@ resource "google_compute_subnetwork" "public_subnet" {
   network  = google_compute_network.vpc.id
 }
 
+// Create the Private Subnets
 resource "google_compute_subnetwork" "private_subnet" {
   for_each = { for k,v in var.networking["vpc_private_subnets"] : k => v }
   name     = each.key
@@ -21,6 +24,7 @@ resource "google_compute_subnetwork" "private_subnet" {
   network  = google_compute_network.vpc.id
 }
 
+// Create a Router for our Private Subnets Egress
 resource "google_compute_router" "nat_router" {
   name    = "outbound-nat-router"
   region   = var.google_project_region
@@ -30,6 +34,7 @@ resource "google_compute_router" "nat_router" {
   }
 }
 
+// Associate NAT to the previously created router
 resource "google_compute_router_nat" "outbound_nat" {
   name                                = "outbound-nat-router"
   router                              = google_compute_router.nat_router.name
@@ -37,6 +42,7 @@ resource "google_compute_router_nat" "outbound_nat" {
   nat_ip_allocate_option              = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat  = "LIST_OF_SUBNETWORKS"
 
+  // Use a Dynamic to create a list of ALL private CIDRs
   dynamic "subnetwork" {
     for_each = var.networking["vpc_private_subnets"]
     content {
@@ -51,8 +57,10 @@ resource "google_compute_router_nat" "outbound_nat" {
   }
 }
 
-###############################################################################
+#########################################################################
 ### FIREWALL
+// Create the INGRESS firewall for ingress TCP/22 and ICMP 
+// for the Bastion and internal hosts.
 resource "google_compute_firewall" "bastion" {
   name = "bastion-firewall"
   network = google_compute_network.vpc.name
@@ -70,6 +78,7 @@ resource "google_compute_firewall" "bastion" {
   }
 }
 
+// Create the EGRESS firewall for the internal K3s workers to 
 resource "google_compute_firewall" "k3s_worker" {
   name = "k3s-worker"
   network = google_compute_network.vpc.name
@@ -84,8 +93,9 @@ resource "google_compute_firewall" "k3s_worker" {
   }
 }
 
-###############################################################################
+#########################################################################
 ### Virtual Machines
+// Create the Bastion VM on the PUBLIC subnet
 resource "google_compute_instance" "bastion" {
   name = "bastion"
   machine_type = var.bastion_settings["vm_type"]
@@ -113,6 +123,7 @@ resource "google_compute_instance" "bastion" {
   }
 }
 
+// Create the internal VM for K3s on the PRIVATE subnet
 resource "google_compute_instance" "k3s_worker" {
   name = "k3s-worker"
   machine_type = var.k3s_settings["vm_type"]
